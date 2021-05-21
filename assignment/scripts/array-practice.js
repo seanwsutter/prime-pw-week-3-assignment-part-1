a346eacbc6c5dd3c3df58cd96a56e324fcbc9dc5
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2021-05-20 23:15:51 -0500

    Added Notes/questions

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -3,9 +3,9 @@ let arrayName = ['coke', 'pepsi', 'sprite'] - coke[0], pepsi[1], sprite[2]
 arrayName.length = 4 - highest index is always 1 less than length
 arrayName.indexOf('x') - checks if variable is in array, -1 means not in array.
 arrayName.push('x') - adds something to END of array
-arrayName.pop('x') - removes something from END of array
+arrayName.pop() - removes something from END of array
 arrayName.unshift('x') - adds something to BEGINNING of array
-arrayName.shift('x') - removes something from BEGINNING of array
+arrayName.shift() - removes something from BEGINNING of array
 
 LOOPS - repeated actions, block of code, depends on conditional being true
 while loop - will continue to run repeated action while condition is true
@@ -37,7 +37,7 @@ console.log('Animals are: ', animalArray);
 let favFoods = ['pizza', 'tacos', 'bacon', 'chicken', 'steak']
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
-console.log('Some favorite foods: ', favFoods);
+console.log('Some favorite foods in favFoods array: ', favFoods);
 
 // 2. Array.length - tells you how many items are in the array
 console.log('--- 2. Length of an array ---');
@@ -46,7 +46,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-console.log('Number of favorite foods:', favFoods.length);
+console.log('Number of favorite foods in favFoods array:', favFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -62,8 +62,7 @@ console.log('Last animal is:', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-console.log('Last animal is:', animalArray[animalArray.length - 1]); 
-
+console.log('Last animal is:', animalArray[animalArray.length - 1]);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
@@ -73,8 +72,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
-
+favFoods.push('pasta');
+console.log('Added new food to end of favFoods array', favFoods);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -83,19 +82,31 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
+let removedFood = favFoods.pop();
 
+console.log('Removed last item in favFoods array:', removedFood);
+console.log('Foods currently in favFoods array:', favFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favFoods.unshift('pie')
+console.log('Added food to beginning of favFoods array:', favFoods);
+console.log(`Added food to beginning of favFoods array: ${favFoods}`);
+// QUESTION: I have a keyboard plugged into my mac and when I try to use the 'tilde' key
+// it types '§' instead of ``. Will look for a fix later
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
-console.log('Removed the first animal', removedAnimal);
-console.log('The animals are now', animalArray);
+console.log('Removed the first animal:', removedAnimal);
+console.log('The animals are now:', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+removedFood = favFoods.shift();
+console.log('Removed food at beginning of favFoods array:', removedFood);
+console.log(`Foods currently in favFoods array: ${favFoods}`);
+console.log('Foods currently in favFoods array:', favFoods);
+// QUESTION: When is the appropiate time to use these different methods of console.logging
